bf578835ae1412ba362e334fc70d48922f31137b
syeimee <148049815+syeimee@users.noreply.github.com>
2024-09-11 22:31:07 +0900

    COPYボタンの実装

--- NextJS/nextjs-rails/frontend/src/components/MarkdownViewer.tsx
+++ NextJS/nextjs-rails/frontend/src/components/MarkdownViewer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 interface MarkdownViewerProps {
   html: string;
@@ -6,10 +6,23 @@ interface MarkdownViewerProps {
 }
 
 const MarkdownViewer: React.FC<MarkdownViewerProps> = ({ html, url }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = ()=>{
+    navigator.clipboard.writeText(html).then(() =>{
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    });    
+  }
+
   return (
-    <div>
-      <h2>出力結果</h2>
-      <div className="markdown-content" dangerouslySetInnerHTML={{ __html: html }} />
+    <div className='items-end justify-end'>
+      <h2 className='text-left'>出力結果</h2>
+      <button 
+        onClick={handleCopy} 
+        className={`px-4 py-2 rounded ${copied ? 'bg-green-500' : 'bg-blue-500 hover:bg-blue-700'} text-white transition`}
+      >COPY</button>
+      <div className="markdown-content text-left bg-slate-700" dangerouslySetInnerHTML={{ __html: html }} />
       <p>引用: {url}</p>
     </div>
   );
