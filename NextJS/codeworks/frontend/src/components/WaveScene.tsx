be12f5213b2850a81db451958a8c46f3c5ce43c4
syeimee <148049815+syeimee@users.noreply.github.com>
2024-10-01 23:06:08 +0900

    マテリアルの実装

--- NextJS/codeworks/frontend/src/components/WaveScene.tsx
+++ NextJS/codeworks/frontend/src/components/WaveScene.tsx
@@ -1,28 +1,28 @@
 'use client';
-import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import { useMemo, useRef } from 'react';
-import { Color, ShaderMaterial, Mesh, DoubleSide, Vector2, TextureLoader} from 'three'; // BoxBufferGeometryを追加
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Suspense, useMemo, useRef } from 'react';
+import { Color, ShaderMaterial, Mesh, DoubleSide, Vector2} from 'three'; // BoxBufferGeometryを追加
 
 import vertexShader from './shaders/vertex.glsl';
 import fragmentShader from './shaders/fragment.glsl';
 
-import jpFlag from "./textures/jp-flag.png"
+// import jpFlag from '@/components/textures/jp-flag.png'
 
 const Box = () => {
   // Mesh型を指定してuseRefを定義
   const mesh = useRef<Mesh>(null);
-  const flagTexture = useLoader(TextureLoader, jpFlag as unknown as string); // テクスチャを読み込む
+  // const flagTexture = useLoader(TextureLoader, jpFlag as unknown as string); // テクスチャを読み込む
   const material = useMemo(() => {
     return new ShaderMaterial({
-      vertexShader,
-      fragmentShader,
-      transparent: true, //透明度を有効
+      vertexShader: vertexShader, 
+      fragmentShader: fragmentShader,
+      // transparent: true, //透明度を有効
       side: DoubleSide, //裏側まで着色
       uniforms: {
         uFrequency: { value: new Vector2(5, 10) }, // 位相のグローバル変数
         uColor: { value: new Color("pink") }, // ユニフォームの追加
         uTime: {value: 0}, //時間のグローバル変数
-        uTexture:{value: flagTexture} //テクスチャのグローバル変数
+        // uTexture:{value: flagTexture} //テクスチャのグローバル変数
       },
     });
   }, []);
@@ -43,11 +43,14 @@ const Box = () => {
 
 const App = () => {
   return (
-    <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
+    <Canvas 
+      camera={{ position: [0, 0, 5], fov: 75 }}>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box />
-    </Canvas>
+      <Suspense >
+        <Box />
+      </Suspense>    
+</Canvas>
   );
 };
 
--- NextJS/codeworks/frontend/src/components/shaders/fragment.glsl
+++ NextJS/codeworks/frontend/src/components/shaders/fragment.glsl
@@ -1,11 +1,18 @@
 uniform vec3 uColor;
-uniform sampler2D uTexture;
+// uniform sampler2D uTexture;
 
 varying float vElevation;
 varying vec2 vUv;
 
-void main(){
-    vec4 textureColor = texture2D(uTexture, vUv);
-    textureColor.rgb *= vElevation * 2.5 +0.7;
-    gl_FragColor = textureColor;
+void main() {
+    // vec3型のuColorをvec4に変換
+    vec4 color = vec4(uColor, 1.0);
+    
+    // テクスチャの代わりにuColorを使用
+    // vec4 textureColor = texture2D(uTexture, vUv);
+    
+    // 色にvElevationに基づいた変調を適用
+    color.rgb *= vElevation * 2.5 + 0.7;
+    
+    gl_FragColor = color;
 }
