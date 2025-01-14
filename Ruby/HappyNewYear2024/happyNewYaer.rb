f7149eaa4655bf5ea258371f2d8ad5ed014d7215
syeimee <148049815+syeimee@users.noreply.github.com>
2025-01-14 22:12:01 +0900

    springbootの環境構築

--- Ruby/HappyNewYear2024/happyNewYaer.rb
+++ Ruby/HappyNewYear2024/happyNewYaer.rb
@@ -5,7 +5,7 @@ def flowing_ascii_art_with_snake(text, font: 'slant')
   artii = Artii::Base.new(font: font)
   ascii_art = artii.asciify(text)
 
-  snake_ascii_art = <<~ASCII.split("\n")
+  snake_ascii_art = <<~ASCII
     　 ＿＿
    　／・・＼
    　L_＿　 ｜
@@ -13,25 +13,34 @@ def flowing_ascii_art_with_snake(text, font: 'slant')
     　￣| /  /|
     　　ヽ二二ノ
   ASCII
+  snake_ascii_art = snake_ascii_art.split("\n")
 
   ascii_art_lines = ascii_art.split("\n")
   max_text_width = ascii_art_lines.map(&:length).max
-  max_snake_width = snake_ascii_art.map(&:length).max
-  total_width = max_text_width + max_snake_width
-
   console_width = `tput cols`.to_i
   colors = [:red, :green, :yellow, :blue, :magenta, :cyan]
 
-  20.times do |offset|
-    ascii_art_lines.each_with_index do |line, index|
-      # 画面の幅を超えないように調整
-      left_part = (' ' * offset + line).slice(0, console_width)  # テキスト部分
-      snake_part = snake_ascii_art[index] || ''                    # 蛇部分
-      snake_display = (' ' * [offset - max_text_width, 0].max + snake_part).slice(0, console_width)  # 蛇部分
+  loop do
+    (0..console_width + max_text_width).each do |offset|
+      system('clear') # 画面をクリア
+      ascii_art_lines.each_with_index do |line, index|
+        # メッセージ部分をスライドさせる
+        left_part = (' ' * [console_width - offset, 0].max + line).slice(0, console_width)
+        puts left_part.colorize(colors.sample)
+      end
 
-      puts left_part.colorize(colors.sample) + snake_display.colorize(colors.sample)
+      # 蛇をメッセージの最後の行に追従させる
+      snake_offset = [console_width - offset, 0].max
+      snake_ascii_art.each_with_index do |snake_line, index|
+        snake_line_display = (' ' * snake_offset + snake_line).slice(0, console_width)
+        puts snake_line_display.colorize(colors.sample)
+      end
+
+      sleep(0.05) # アニメーションの速度を調整
     end
   end
 end
 
 flowing_ascii_art_with_snake("Happy New Year 2025", font: 'slant')
+
+
