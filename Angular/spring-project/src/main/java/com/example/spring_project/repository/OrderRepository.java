1b690540fec6a0967cf1b3f0ce2fc0bf1f4669d5
syeimee <148049815+syeimee@users.noreply.github.com>
2025-06-01 11:10:37 +0900

    MemberStatusのentityとrepositoryを作成

--- Angular/spring-project/src/main/java/com/example/spring_project/repository/OrderRepository.java
+++ Angular/spring-project/src/main/java/com/example/spring_project/repository/OrderRepository.java
@@ -1,10 +1,13 @@
 package com.example.spring_project.repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page; 
 import org.springframework.data.domain.Pageable; 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.spring_project.entity.OrderHistory;
@@ -17,4 +20,8 @@ public interface OrderRepository extends JpaRepository<OrderHistory, String>{
     Optional<OrderHistory> findByOrderNumber(String orderNumber);
 
     Page<OrderHistory> findByUserId(UUID userId, Pageable pageable);
+
+    //過去3ヶ月分の合計取得ポイントに基づいて会員ランクを決定する
+    @Query("SELECT SUM(o.totalPrice) / 100 FROM orderHistory o where o.userId = :userId AND o.orderDate >= :since")
+    int sumPointsForUserSince(@Param("userId") UUID userId, @Param("since") LocalDateTime since);
 }
