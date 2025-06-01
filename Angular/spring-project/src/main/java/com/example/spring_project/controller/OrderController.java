396e6b3c9f88a6c472b92520546322ba444ebe68
syeimee <148049815+syeimee@users.noreply.github.com>
2025-06-01 09:01:52 +0900

    ページネーションのcontroller実装

--- Angular/spring-project/src/main/java/com/example/spring_project/controller/OrderController.java
+++ Angular/spring-project/src/main/java/com/example/spring_project/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.example.spring_project.controller;
 
+import java.util.UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +10,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.spring_project.dto.OrderHistoryResponse;
 import com.example.spring_project.dto.OrderRequest;
 import com.example.spring_project.dto.OrderResponse;
+import com.example.spring_project.entity.OrderHistory;
 import com.example.spring_project.service.OrderService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController //REST APIであることを明記
@@ -37,6 +44,23 @@ public class OrderController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
         }
     }
+
+    @GetMapping
+    public ResponseEntity<OrderHistoryResponse> getOrderHisotry(
+        @RequestParam UUID userId, 
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size
+    ){
+        try{
+            OrderHistoryResponse orderHistoryResponse = orderService.getOrderHistory(userId, page, size);
+            return ResponseEntity.ok(orderHistoryResponse);
+        }catch(Exception e){
+            logger.error("ERROR OCCURRED FETCHING ORDER HISTORY", e);
+            OrderHistoryResponse errorResponse = new OrderHistoryResponse(null,null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
+    
     
 
 }
