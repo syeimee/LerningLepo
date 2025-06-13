d855687b2646f18af816960aec03b8fd0a3ee5ca
syeimee <yuuguiter1@gmail.com>
2025-06-13 21:45:38 +0900

    20250612_apacheアプリケーションを作ってみる

---
+++ VirtualMachine/20250612_apacheアプリケーションを作ってみる/src/demoapp/WEB-INF/classes/HelloWorld.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloWorld extends HttpServlet {
+private static final long serialVersionUID = 1L;
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+  throws IOException, ServletException
+  {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>HelloWorld</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>HelloWorld</h1>");
+    out.println("</body>");
+    out.println("</html>");
+  }
+}
