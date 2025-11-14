c633f4a353278cf2f017a89e609ab2efad3758b5
Yu Misei <yuuguiter1@gmail.com>
2025-11-15 08:31:13 +0900

    lerning: インターフェイスの分離

--- DesignPattern/ts-design-pattern/03-solid-principle/interfaceSegregation/goodExample.ts
+++ DesignPattern/ts-design-pattern/03-solid-principle/interfaceSegregation/goodExample.ts
@@ -1,1 +1,54 @@
 export {};
+interface Flyable {
+    fly();
+}
+
+interface Moveable {
+    start();
+    stop();
+}
+
+interface Vehivle {
+    name: string;
+    color: string;
+}
+
+class Airplane implements Flyable, Moveable, Vehivle {
+    name: string;
+    color: string;
+
+    start(){
+        console.log('start!');
+    }
+
+    stop(){
+        console.log('stop!');
+    }
+
+    fly(){
+        console.log('fly!');
+    }
+}
+
+class Car implements Moveable, Vehivle {
+    name: string;
+    color: string;
+
+    start(){
+        console.log('start!');
+    }
+
+    stop(){
+        console.log('stop!');
+    }
+}
+
+function run(){
+    const v1 = new Airplane();
+    const v2 = new Car();
+
+    v1.fly();
+    v2.start()
+}
+
+run();
