5a7f1d1080ae7743cf7039f30af05b4f0b61220e
Yu Misei <yuuguiter1@gmail.com>
2025-11-15 09:45:16 +0900

    learning: 依存性逆転の法則

--- DesignPattern/ts-design-pattern/03-solid-principle/dependencyInversion/goodExample.ts
+++ DesignPattern/ts-design-pattern/03-solid-principle/dependencyInversion/goodExample.ts
@@ -1,1 +1,62 @@
-export {};
+export { };
+
+class User { }
+
+interface IUserService {
+    create(user: User): User;
+    findById(id: string): User;
+}
+
+class UserController {
+    constructor(private userService: IUserService){
+        this.userService = userService;
+    }
+
+    create(user: User): User {
+        return this.userService.create(user);
+    }
+
+    findById(id: string): User {
+        return this.userService.findById(id);
+    }
+}
+
+interface IUserRepository {
+    create(user: User): User;
+    findById(id: string): User;
+}
+
+class UserService implements IUserService {
+    constructor(private userRdbRepository: IUserRepository){
+        this.userRdbRepository = userRdbRepository;
+    }
+
+    create(user: User): User {
+        return this.userRdbRepository.create(user);
+    }
+
+    findById(id: string): User {
+        return this.userRdbRepository.findById(id);
+    }
+}
+
+class UserRdbRepository implements IUserRepository{
+    create(user: User): User {
+        console.log('RDBにUserを登録');
+        return user;
+    }
+
+    findById(id: string): User {
+        console.log(`ID: ${id}のユーザーを検索`);
+        return new User();
+    }
+}
+
+function run() {
+    const repositoruy = new UserRdbRepository();
+    const service = new UserService(repositoruy);
+    const userController = new UserController(service);
+    userController.findById('123');
+}
+
+run();
