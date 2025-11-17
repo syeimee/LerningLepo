2850eddad46781b6f5588f05733f1ef5faa947e8
Yu Misei <yuuguiter1@gmail.com>
2025-11-17 23:02:55 +0900

    learning: テンプレートメソッドパターン

--- DesignPattern/ts-design-pattern/04-design-pattern-part1/template_method/templateMethod.ts
+++ DesignPattern/ts-design-pattern/04-design-pattern-part1/template_method/templateMethod.ts
@@ -1,2 +1,45 @@
 export {}
 
+abstract class TestTemplate {
+    test(){
+        this.setup();
+        this.execute();
+        this.teardown();
+
+    }
+    abstract setup();
+    abstract execute();
+    teardown(){
+        console.log("teardown");
+    }
+}
+
+class ItemServiseTest extends TestTemplate{
+    setup(){
+        console.log("setup ItemServiseTest");
+    }
+    execute(){
+        console.log("execute ItemServiseTest");
+    }
+}
+
+class UserServiceTest extends TestTemplate{
+    setup(){
+        console.log("setup UserServiceTest");
+    }
+    execute(){
+        console.log("execute UserServiceTest");
+    }
+}
+
+function run(){
+    const itemServiseTest = new ItemServiseTest();
+    const userServiceTest = new UserServiceTest();
+
+    itemServiseTest.test();
+    console.log("");
+    userServiceTest.test();
+
+}
+
+run();
