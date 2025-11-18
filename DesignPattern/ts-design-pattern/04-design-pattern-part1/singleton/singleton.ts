20a53bad34b8e843a6ee479d35bb4abb26bd5adb
Yu Misei <yuuguiter1@gmail.com>
2025-11-18 23:23:23 +0900

    learning: シングルトン

--- DesignPattern/ts-design-pattern/04-design-pattern-part1/singleton/singleton.ts
+++ DesignPattern/ts-design-pattern/04-design-pattern-part1/singleton/singleton.ts
@@ -1,2 +1,38 @@
 export {}
 
+class Logger {
+    private static instance: Logger;
+
+    //　外部からインスタンス化を防止（シングルトン）
+    private constructor(){};
+
+    static getInstance(){
+        if(!this.instance){
+            this.instance = new Logger();
+        }
+
+        return this.instance;
+    }
+
+    output(content: string){
+        const now = new Date();
+        console.log(`${now.toLocaleString('ja-JP')}: ${content}`);
+    }
+}
+
+class Test {};
+
+function run(){
+    const test1 = new Test();
+    const test2 = new Test();
+    console.log("TESTクラス", test1 === test2);//false 別のインスタンス
+
+    const logger1 = Logger.getInstance();
+    const logger2 = Logger.getInstance();
+    console.log("Loggerクラス", logger1 === logger2);//true 同一のインスタンス
+
+    logger1.output("logger1のログ");
+    logger2.output("logger2のログ");
+}
+
+run();
